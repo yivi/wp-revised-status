bbac04f5332324a5d1b6d1f2fdff6e46090f66ab
yivi <ivan@ojiva.es>
2015-07-19 13:16:58 +0200

    revised gulpfile, now bumps properly

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,11 @@ var gulp = require('gulp'),
     bump = require('gulp-bump'),
     git = require('gulp-git'),
     filter = require('gulp-filter'),
-    tag = require('gulp-tag-version'),
     fs = require('fs'),
-    readme = require('gulp-readme-to-markdown')
-dbg = require('gulp-debug');
+    readme = require('gulp-readme-to-markdown'),
+    merge = require('merge-stream'),
+    dbg = require('gulp-debug');
 
-
-var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-var getPackageJson = function () {
-    return pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-};
 
 var bumpReadme = function (version, type) {
     util.log('Starting to bump from ' + version);
@@ -46,59 +41,40 @@ var bumpReadme = function (version, type) {
     return version;
 };
 
-gulp.task('getPackageJson', function (cb) {
-    pkg = getPackageJson();
-    cb(err);
-});
 
 
 function inc(importance) {
 
-    version = pkg.version.replace('v', '');
-    nextV = bumpReadme(version, importance);
+    var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+    var version = pkg.version.replace('v', '');
+    var nextV = bumpReadme(version, importance);
 
     onlyJson = filter('package.json');
     onlyReadme = filter('readme.txt');
 
     // get all the files to bump version in
 
-    return gulp.src(['package.json', 'readme.txt', 'README.md' ])
-        .pipe(onlyJson)
-        // bump the version number in those files
-        .pipe(bump({type: importance}))
-        // save it back to filesystem
-        .pipe(gulp.dest('./'))
-
-
-        .pipe(onlyJson.restore())
-        .pipe(dbg({title: 'After Json Restore'}))
-
-        .pipe(onlyReadme)
-        .pipe(dbg({title: 'After Readme Filter '}))
+    readme = gulp.src('readme.txt')
         .pipe(replace('Stable tag: ' + version, 'Stable tag: ' + nextV))
         .pipe(gulp.dest('./'))
-
         .pipe(readme())
-        .pipe(dbg({title: 'After Readme Process '}))
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest('./'));
+
+    json = gulp.src('package.json')
+        .pipe(bump({type: importance}))
+        .pipe(gulp.dest('./'));
 
 
-        // restore the original filter, so I add three files to git
-        .pipe(onlyReadme.restore())
+    readmeme = gulp.src([ 'README.md', 'readme.txt']);
 
-        .pipe(dbg({title: 'After Readme Restore'}))
+    return merge([json, readmeme])
         .pipe(git.add())
-
         // commit the changed version number
-        .pipe(git.commit('bump to v' + nextV ))
-        .pipe(git.tag('v' + nextV, 'Bumping from ' + version + 'to ' + nextV), function(err) {
+        .pipe(git.commit('bump to v' + nextV))
+        .pipe(git.tag('v' + nextV, 'Bumping from ' + version + 'to ' + nextV), function (err) {
             if (err) throw err;
         });
 
-    // read only one file to get the version number
-    // .pipe(filter('package.json'))
-    // **tag it in the repository**
-    // .pipe(tag);
 }
 
 gulp.task('patch', function () {
